6e8db7dfd194960d372cbdae980dc45189201ef3
Daria <DoshikSmirnoff@gmail.com>
2026-01-16 16:44:41 +0300

    New commit

--- Structures/Dinamic_Array.py
+++ Structures/Dinamic_Array.py
@@ -63,7 +63,8 @@ class Vector(ABC):
 
     def __init__(self, capacity: int = 4, array: list[Any] | None = None):
         """
-        capacity -
+        capacity - объем выделенной памяти, по умолчанию 4, но можно передать свое значение
+        array - передается ли массив при создании экземпляра. По умолчанию не передается
         """
         if capacity <= 0:
             raise NotImplementedError("Память должна быть больше 0")
@@ -94,22 +95,24 @@ class Vector(ABC):
     def __contains__(self, item):
         return item in self.array[:self.size]
 
+    # СКОРРЕКТИРОВАЛА
     @abstractmethod
-    def __setitem__(self, index, value): # дописать про отриц индексы в обрутную сторону (лен + 1)
-        if self.size == 0 or index > self.size:
+    def __setitem__(self, index, value): # Надо ли писать про добавление по несуществующему индексу типа insert?
+        if self.size == 0 or index > self.size or index < -(len(self.array)):
             raise IndexError
         else:
             self.array[index] = value
 
+    # СКОРРЕКТИРОВАЛА
     @abstractmethod
-    def __getitem__(self, index): # дописать про отриц индексы в обрутную сторону (лен + 1)
-        if self.size == 0 or index > self.size:
+    def __getitem__(self, index):
+        if self.size == 0 or index > self.size or index < -(len(self.array)):
             raise IndexError
         else:
             return self.array[index]
 
     @abstractmethod
-    def __delitem__(self, index): # Добавить обработку индексов, дописать про отриц индексы в обрутную сторону (лен + 1)
+    def __delitem__(self, index):
        if index > len(self.array) or index < -(len(self.array)):
            raise IndexError
        else:
@@ -124,48 +127,86 @@ class Vector(ABC):
             self.capacity *= 2
             self.array += (self.capacity - self.size) * [None]
 
+    # СКОРРЕКТИРОВАЛА
     @abstractmethod
     def append(self, item): # Переписать условия проверки - вынести до += , довыделить память по аналогии с созданием массива (доп метод)
+        start_len = self.size
         self.increase_memory()
-        for elem in range(len(self.array)):
-            if self.array[elem] is None:
-                self.array[elem] = item
-                break # первый None замена на итем
-        self.size += 1 # переписать без цикла с использованием self.size
+        self.array[start_len + 1] = item
 
-# СКОРРЕКТИРОВАЛА, переписать по аналогии аппенд
+        # for elem in range(len(self.array)):
+        #     if self.array[elem] is None:
+        #         self.array[elem] = item
+        #         break # первый None замена на итем
+        # self.size += 1 # переписать без цикла с использованием self.size
+
+# НЕ ПОНИМАЮ как переписать по аналогии аппенд
     @abstractmethod
     def insert(self, index, item): # Добавить часть про size, соотношение с capacity, циклом сдвинуть часть массива
         unchange_part_left = self.array[:index] # неизменяемая часть массива (которая не сдвигается) слева
         unchange_part_right = self.array[index + 1:] # неизменяемая часть массива справа
+        self.increase_memory()
         past_elem = self.array[index] # старый, заменяемый элемент, по индексу
         self.array[index] = item # замена элемента по индексу на новый
         new_elem = self.array[index] # новый элемент, на который меняли
         unchange_part_left += [new_elem, past_elem, unchange_part_right]
 
-# СКОРРЕКТИРОВАЛА, переписать как делайтем
+    # СКОРРЕКТИРОВАЛА
     @abstractmethod
     def remove(self, item):
-        elem_index = self.array.index(item) # переписать поиск при помощи алгоритмов
-        unchange_part_left = self.array[:elem_index]
-        unchange_part_right = self.array[elem_index + 1:]
-        del self.array[elem_index]
+        # elem_index = self.array.index(item) # переписать поиск при помощи алгоритмов
+        # unchange_part_left = self.array[:elem_index]
+        # unchange_part_right = self.array[elem_index + 1:]
+        # del self.array[elem_index]
+        #
+        # unchange_part_left += unchange_part_right
 
-        unchange_part_left += unchange_part_right
+        for index in range(self.size): # Использовала линейный поиск, тк неясно упорядочен ли массив
+            if self.array[index] == item:
+                self.array = self.array[:index]
+                unchange_part_right = self.array[index + 1:]
+                self.array += unchange_part_right
+                break
 
+    # СКОРРЕКТИРОВАЛА
     @abstractmethod
     def pop(self, index=-1): # Добавить, если индекс != -1
-        if len(self.array) > 0:
+        # if len(self.array) <= 0:
+        #     last_item = self.array[index]
+        #     del self.array[index]
+        #     return last_item
+        # else:
+        #     raise IndexError
+
+        if index == -1:
             last_item = self.array[index]
             del self.array[index]
             return last_item
+
+        if -(self.size) <= index < self.size: # Почему подчеркивается значение с минусом?
+            del_item = self.array[index]
+            self.array = self.array[:index]
+            unchange_part_right = self.array[index + 1:]
+            self.array += unchange_part_right
+            return del_item
         else:
             raise IndexError
 
-# СКОРРЕКТИРОВАЛА, через вайл инкриз мемори вызывать
+
+# СКОРРЕКТИРОВАЛА, логику в заметках расписала, чтобы не забыть
     @abstractmethod
     def extend(self, new_array):
-        self.array += new_array
+
+        start_len = self.size
+        necessary_capacity = self.size + len(new_array)
+
+        while self.size < necessary_capacity:
+            self.increase_memory()
+
+        for index in range(len(self.array[:start_len + 1]), necessary_capacity + 1):
+            new_array_index = 0
+            self.array[index] = new_array[new_array_index]
+            new_array_index += 1
 
     @abstractmethod
     def __iter__(self):
